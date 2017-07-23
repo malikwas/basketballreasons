b08e72240516b4143f3691898415d4502c7f2c68
wmlk <malik.w.as@gmail.com>
2017-07-23 18:12:20 -0400

    Refer to documentation comment for API.

--- server/api/data/data.js
+++ server/api/data/data.js
@@ -1,3 +1,6 @@
+// FOR DOCUMENTATION ON EACH METHOD, REFER TO
+// https://github.com/kshvmdn/nba.js/blob/master/docs/api/DATA.md
+
 const express = require('express');
 const data = require('nba.js').data;
 
--- server/api/stats/stats.js
+++ server/api/stats/stats.js
@@ -1,3 +1,6 @@
+// FOR DOCUMENTATION ON EACH METHOD, REFER TO
+// https://github.com/kshvmdn/nba.js/blob/master/docs/api/STATS.md
+
 const express = require('express');
 const stats = require('nba.js').stats;
 
