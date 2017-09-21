a525207ee0f54c1e0c8b14f879a59fecf2051339
wmlk <malik.w.as@gmail.com>
2017-09-21 18:59:28 -0400

    Fixed dumb error.

--- public/index.js
+++ public/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(path.join(__dirname, '../build');
+app.use(express.static(path.join(__dirname, '../build')));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
