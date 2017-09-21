04898d3e693561af3e1f6a356dda160d8edf78d8
wmlk <malik.w.as@gmail.com>
2017-09-21 19:24:15 -0400

    Commit to see if it works.

--- public/index.js
+++ public/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5432));
 app.use(express.static(path.join(__dirname, '../build')));
 
 app.listen(app.get('port'), function() {
