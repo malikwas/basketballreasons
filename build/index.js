cb30b846bc212b83ff3ef165dbf99a1a8c0caa12
wmlk <malik.w.as@gmail.com>
2017-09-21 16:34:58 -0400

    Serve using express.

---
+++ build/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.get('/', function(request, response) {
+  response.render('index.html');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
