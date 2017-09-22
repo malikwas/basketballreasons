182af573243da710031f5b95b5a45bb5546bec66
wmlk <malik.w.as@gmail.com>
2017-09-22 13:40:00 -0400

    Function at set interval to keep API dyno awake.

--- public/index.js
+++ public/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const axios = require('axios');
 
 app.set('port', (process.env.PORT || 5432));
 app.use(express.static(path.join(__dirname, '..', 'build')));
@@ -11,3 +12,10 @@ app.listen(app.get('port'), function() {
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
 });
+
+// To keep API awake
+setInterval(() => {
+  axios.get('http://api.basketballreasons.io/static/data/calendar/playoffs_2017.json').then(() => {
+    console.log('Kept API alive');
+  })
+})
