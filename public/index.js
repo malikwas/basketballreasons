18338a2fa5a8dddb238e3dd43ca04af3f27e0f1c
wmlk <malik.w.as@gmail.com>
2017-09-22 14:25:30 -0400

    Keep API alive every 5 mintues.

--- public/index.js
+++ public/index.js
@@ -13,11 +13,11 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
 });
 
-// To keep API awake
+// To keep API awake every 5 minutes
 setInterval(() => {
   axios.get('http://api.basketballreasons.io/static/data/calendar/playoffs_2017.json').then(() => {
     console.log('Kept API alive');
   }).catch(() => {
   	console.log('API is down');
-  })
-})
+  });
+}, 300000)
