9dd48b611a7542214f646f93d9768841b16bcedb
wmlk <malik.w.as@gmail.com>
2019-08-02 16:59:32 -0400

    Setup for v2

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+import { Label } from 'semantic-ui-react';
+import './App.css';
+
+function App() {
+  return (
+      <Label>Message</Label>
+  );
+}
+
+export default App;
